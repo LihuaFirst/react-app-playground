0f4ed237449187e2bdd8ea1070011948a7bce28e
DESKTOP-QC846T0\Lihua First <lihua.first@gmail.com>
2019-11-20 23:34:46 -0500

    Create 9-checkbox.js

---
+++ src/tutorials/9-checkbox.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class Reservation extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isGoing: true,
+            numberOfGuest: 2
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type == "checkbox" ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    render() {
+        return (
+            <form>
+                <label>
+                    Is Going:
+                    <input type="checkbox" name="isGoing" checked={this.state.isGoing} onChange={this.handleInputChange} />
+                </label>
+                <br />
+                <label>
+                    Number of Guests:
+                    <input type="number" name="numberOfGuest" value={this.state.numberOfGuest} onChange={this.handleInputChange} />
+                </label>
+            </form>
+        );
+    }
+
+}
+
+export default Reservation;
